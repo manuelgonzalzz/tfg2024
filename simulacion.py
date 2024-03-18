dae3a2c7aed49a7995e76c624edea4cef57b5033
manuelgonzalez <manuelgonzalezcanizares@gmail.com>
2024-03-18 09:39:44 +0100

    githbub 180324

--- simulacion.py
+++ simulacion.py
@@ -88,7 +88,7 @@ def dist(N_pix,C_med,num_cuentas,filas_eliminadas,columnas_eliminadas):
 
 
 
-#Introduciendo los defectos en las matrices
+#Introduciendo los defectos en las matrices sin defecto
 def matriz_defectos(t,k,N_pix,C_med,fov):
     matriz = matriz_uniform(N_pix,C_med,fov)
     n=int(t*(N_pix/64))
